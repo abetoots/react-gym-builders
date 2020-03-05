0f0ad124a8033ca970ba230cb3f7456cfb113c9a
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-05 16:26:53 +0800

    constants and mutations moved to constants file
    -comment too long for tokenCache
    -mutations that depend on dispatch should let dispatch handle UI hints
    -mutations that do not receive dispatch should have state to handle UI hints
    -convert useLazyLoginMutation to async/await
    -move refresh helpers below useRefreshToken
    -better naming
    -better error and loading state
    -add cleaning up of localstorage on error
    -subscribe to success and error states and set loading accordingly

--- src/hooks/wp-graphql-token.js
+++ src/hooks/wp-graphql-token.js
@@ -1,127 +1,54 @@
 import { useState, useEffect } from "react";
 
-//Follow this auth flow : https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#jwt_persist
-// For WPGraphQl , simplified : https://github.com/NeverNull/gatsby-apollo-wpgraphql-jwt-starter/issues/1
+import {
+  JWT_AUTH_EXPIRATION,
+  AUTH_TOKEN,
+  getRefreshMutation
+} from "../misc/constants";
+import { setupLocalStorage, cleanupLocalStorage } from "../misc/util";
 
-//!Be careful when setting tokenCache as some redirects depend on this
-//Example: we set the tokenCache causing a redirect, problem is, we write some code after it like setSomething()
-// this will trigger an error since we have already been redirected (component is unmounted already) therefore
-// causing something like "Warning: Can't perform a React state update on an unmounted component"
+//Follow this auth flow : https://hasura.io/blog/best-practices-of-using-jwt-with-graphql/#jwt_persist
+//For WPGraphQl , simplified : https://github.com/NeverNull/gatsby-apollo-wpgraphql-jwt-starter/issues/1
+
+//!Do not use as hints for UI updates
 export const tokenCache = {};
 
-export const EXPDATE = "expirationDate";
-export const REFTOKEN = "refreshToken";
-
-const refreshMutation = `
-    mutation RefreshToken {
-      refreshJwtAuthToken(
-        input: { clientMutationId: "", jwtRefreshToken: "${localStorage.getItem(
-          REFTOKEN
-        )}" }
-      ) {
-        authToken
-      }
-    }
-  `;
-
 export const useLazyLoginMutation = dispatch => {
-  let query;
-  const setQuery = customQuery => {
-    query = customQuery;
-  };
-
-  const fetchToken = () => {
-    fetch(BASE_URL + "graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        query: query
-      })
-    })
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw res.json();
-        }
-      })
-      .then(data => {
-        console.log("FetchAPI: Done!", [data]);
-        localStorage.setItem(REFTOKEN, data.data.login.refreshToken);
-        localStorage.setItem(EXPDATE, data.data.login.user.jwtAuthExpiration);
-        tokenCache.token = data.data.login.authToken;
-        silentlyRefresh();
-        dispatch("LOGIN_SUCCESS", data);
-      })
-      .catch(err => {
-        console.log("FetchAPI: Error!", [err]);
-        dispatch("LOGIN_FAIL", err);
-      });
-  };
-
-  const startLogin = () => {
-    console.log("Fetch API: fetching...");
+  const startLogin = async theQuery => {
+    console.log("[useLazyLoginMutation]: fetching...");
     dispatch("LOGIN_START");
-    fetchToken();
+    try {
+      const resData = await fetch(BASE_URL + "graphql", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          query: query
+        }).then(res => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw res.json();
+          }
+        })
+      });
+
+      console.log("[useLazyLoginMutation]: Done!", resData);
+      setupLocalStorage(resData.data);
+      tokenCache.token = resData.data.login[AUTH_TOKEN];
+      silentlyRefresh();
+      dispatch("LOGIN_SUCCESS", resData.data);
+    } catch (err) {
+      console.log("[useLazyLoginMutation]: Error!", [err]);
+      dispatch("LOGIN_FAIL", {
+        errorDev: err,
+        output: "Login failed! Something went wrong with our servers"
+      });
+    }
   };
 
-  return [startLogin, setQuery];
-};
-
-const setNewExpirationDate = () => {
-  //set the new expiry date as 4.5 minutes from now IN SECONDS since epoch time
-  localStorage.setItem(
-    EXPDATE,
-    new Date(
-      //date now in milliseconds since epoch time + 4.5 minutes in ms
-      new Date().getTime() + 270000
-    ).getTime() / 1000
-  ); // converted to seconds since epoch time)
-};
-
-// A refresh without loading and error hints, simply for background task. We DONT
-// interfere with the UX
-const backgroundRefresh = async () => {
-  console.log("Refreshing in the background ...");
-  try {
-    const res = await fetch(BASE_URL + "graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        query: refreshMutation
-      })
-    }).then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw res.json();
-    });
-    //We got a valid token, user can now have access UI
-    //No more UI loading hint, only silent refreshes from here on out
-    setNewExpirationDate();
-    console.log(res);
-    tokenCache.token = res.data.refreshJwtAuthToken.authToken;
-    silentlyRefresh();
-  } catch (err) {
-    console.log(err);
-    //TODO we dont want to interrupt UX when the background refresh fails
-    //TODO maybe handle this idk like logout
-  }
-};
-
-let timerId;
-let delay;
-const silentlyRefresh = () => {
-  console.log("Silently refreshing ...");
-  //we expect the date to be stored as seconds since epoch time
-  //since settimeout talks in milliseconds, we get the remaining milliseconds (hence multiplying to 1000)
-  //expiration date in ms since epoch - Date now in ms since epoch = remaining milliseconds
-  delay = new Date(localStorage.getItem(EXPDATE) * 1000) - new Date().getTime();
-  timerId = setTimeout(backgroundRefresh, delay);
+  return [startLogin];
 };
 
 /**
@@ -137,19 +64,19 @@ export const useRefreshToken = () => {
     called: false
   });
   const [success, setSuccess] = useState({ successRefresh: false, data: [] });
-  const [errorRefresh, setErrorLogin] = useState("");
+  const [error, setError] = useState({ errorRefresh: false, output: "" });
 
-  // We force the user to wait since a token was found so we give out loading hints.
+  // We force the user to wait since a refresh token was found so we give out loading hints.
   const refreshWithHints = async () => {
     console.log("Actively refreshing ...");
     try {
-      const res = await fetch(BASE_URL + "graphql", {
+      const resData = await fetch(BASE_URL + "graphql", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          query: refreshMutation
+          query: getRefreshMutation()
         })
       }).then(res => {
         if (res.ok) {
@@ -162,27 +89,83 @@ export const useRefreshToken = () => {
       //We got a valid token, user can now have access to UI
       //No more UI loading hint, only silent refreshes from here on out
       setNewExpirationDate();
-      tokenCache.token = res.data.refreshJwtAuthToken.authToken;
+      tokenCache.token = resData.data.refreshJwtAuthToken[AUTH_TOKEN];
       silentlyRefresh();
-      setSuccess({ successRefresh: true, data: res.data });
+      setSuccess({ successRefresh: true, data: resData.data });
     } catch (err) {
-      console.log("Active refresh error: ", [err]);
+      console.log("[useRefreshToken] [refreshWithHints]: error! ", [err]);
       //Maybe invalid refresh token. Redirect to login
+      cleanupLocalStorage();
+      setError({ ...error, errorRefresh: true, output: err });
     }
   };
 
   useEffect(() => {
-    if (success.successRefresh || errorRefresh) {
-      console.log(loading);
-      console.log(success);
+    if (success.successRefresh || error.errorRefresh) {
       setLoading({ ...loading, loadingRefresh: false });
     }
-  }, [success, errorRefresh]);
+  }, [success, error]);
 
   const startRefresh = () => {
     refreshWithHints();
     setLoading({ loadingRefresh: true, called: true });
   };
 
-  return [loading, success, errorRefresh, startRefresh];
+  return [startRefresh, loading, success, error];
+};
+
+const setNewExpirationDate = () => {
+  //set the new expiry date as 4.5 minutes from now IN SECONDS since epoch time
+  localStorage.setItem(
+    JWT_AUTH_EXPIRATION,
+    new Date(
+      //date now in milliseconds since epoch time + 4.5 minutes in ms
+      new Date().getTime() + 270000
+    ).getTime() / 1000
+  ); // converted to seconds since epoch time)
+};
+
+// A refresh without loading and error hints, simply for background task. We DONT
+// interfere with the UX
+const backgroundRefresh = async () => {
+  console.log("Refreshing in the background ...");
+  try {
+    const resData = await fetch(BASE_URL + "graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        query: getRefreshMutation()
+      })
+    }).then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw res.json();
+    });
+    //We got a valid token, user can now have access UI
+    //No more UI loading hint, only silent refreshes from here on out
+    setNewExpirationDate();
+    console.log("[backgroundRefresh]: Done!", [resData]);
+    tokenCache.token = resData.data.refreshJwtAuthToken[AUTH_TOKEN];
+    silentlyRefresh();
+  } catch (err) {
+    console.log("[backgroundRefresh]: Error!", [err]);
+    tokenCache.token = null;
+    cleanupLocalStorage();
+  }
+};
+
+let timerId;
+let delay;
+const silentlyRefresh = () => {
+  console.log("Silently refreshing ...");
+  //we expect the date to be stored as seconds since epoch time
+  //since settimeout talks in milliseconds, we get the remaining milliseconds (hence multiplying to 1000)
+  //expiration date in ms since epoch - Date now in ms since epoch = remaining milliseconds
+  delay =
+    new Date(localStorage.getItem(JWT_AUTH_EXPIRATION) * 1000) -
+    new Date().getTime();
+  timerId = setTimeout(backgroundRefresh, delay);
 };
