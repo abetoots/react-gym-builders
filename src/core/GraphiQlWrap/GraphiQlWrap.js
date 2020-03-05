76b1c76aa9886799cfe017c7e74a34853f5e389f
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-05 16:15:11 +0800

    add todo

--- src/core/GraphiQlWrap/GraphiQlWrap.js
+++ src/core/GraphiQlWrap/GraphiQlWrap.js
@@ -5,7 +5,6 @@ import "graphiql/graphiql.css";
 
 import Layout from "../../components/layout";
 
-//TODO switch to WP GraphiQL just because it's tightly woven with WP GraphQL , this was a nice learning experience though
 const GraphiQlWrap = props => {
   const fetcher = async graphQlParams => {
     const response = await fetch(BASE_URL + "/graphql", {
