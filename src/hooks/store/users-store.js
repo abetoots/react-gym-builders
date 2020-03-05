347d5f20139bcc2cb1d2eb0d9ea355b5ac52b860
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-05 16:08:51 +0800

    add store for user related actions

---
+++ src/hooks/store/users-store.js
@@ -0,0 +1,37 @@
+import { initStore } from "./store";
+
+const initialState = {
+  registeredNewUser: false,
+  registering: false,
+  registered: false,
+  errorRegister: {
+    //errors meant for the developer
+    errorDev: null,
+    //errors meant for the user UI
+    output: ""
+  }
+};
+
+const configureStore = listenerKey => {
+  const actions = {
+    REGISTER_START: () => ({ registering: true }),
+    REGISTER_SUCCESS: () => ({
+      ...initialState,
+      registering: false,
+      registered: true,
+      registeredNewUser: true
+    }),
+    REGISTER_FAIL: (state, errorDispatch) => {
+      console.log("[Dispatch Error]: REGISTER_FAIL ", [errorDispatch.errorDev]);
+      return {
+        ...initialState,
+        errorRegister: { ...state.errorRegister, ...errorDispatch }
+      };
+    },
+    REFETCHED_NEW_USER: () => ({ registeredNewUser: false })
+  };
+
+  initStore(initialState, actions, listenerKey);
+};
+
+export default configureStore;
