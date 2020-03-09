d68db089d47ed1d668f4a9b7daeff07d94c4b17e
Abe Suni M. Caymo <45987443+abetoots@users.noreply.github.com>
2020-03-09 12:42:10 +0800

    handle props better

--- src/components/UI/BurgerMenu/BurgerMenu.js
+++ src/components/UI/BurgerMenu/BurgerMenu.js
@@ -1,22 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./BurgerMenu.scss";
 
-import PropTypes from "prop-types";
+const BurgerMenu = props => {
+  const burgerClasses = ["BurgerMenu"];
 
-const BurgerMenu = props => (
-  <button
-    className={`BurgerMenu ${props.toggled ? "-toggled" : ""}`}
-    aria-expanded={props.toggled}
-    onClick={props.handleClick}
-  >
-    <div className="BurgerMenu__bar -one"></div>
-    <div className="BurgerMenu__bar -two"></div>
-    <div className="BurgerMenu__bar -three"></div>
-    <span className={`BurgerMenu__label ${props.hideLabel ? "-hidden" : ""}`}>
-      Menu
-    </span>
-  </button>
-);
+  if (props.toggled) {
+    burgerClasses.push("-toggled");
+  }
+
+  return (
+    <button
+      className={burgerClasses.join(" ")}
+      aria-expanded={props.toggled}
+      onClick={props.handleClick}
+    >
+      <div className="BurgerMenu__bar -one"></div>
+      <div className="BurgerMenu__bar -two"></div>
+      <div className="BurgerMenu__bar -three"></div>
+      <span className={`BurgerMenu__label ${props.hideLabel ? "-hidden" : ""}`}>
+        Menu
+      </span>
+    </button>
+  );
+};
 
 BurgerMenu.propTypes = {
   handleClick: PropTypes.func.isRequired,
